9b1cd6ee74b84fae6d3c7b545a72d5871f904660
thealienthing <benjamin.stoneking@gmail.com>
2019-08-16 17:33:48 -0600

    Grid object set up.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,74 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class NoteBox extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
+
+  render() {
+    return (
+      <div
+        className="square"
+        onClick={() => this.setState({value: "X"})}>
+        {this.state.value}
+      </div>
+    );
+  }
+}
+
+class GridRow extends React.Component {
+  renderNoteBox() {
+    return <NoteBox Value="X"/>
+  }
+
+  render() {
+    return (
+      <div className="GridRow">
+        <tr>
+          <div class="trDiv">
+          <td>{this.renderNoteBox()}</td>
+          <td>{this.renderNoteBox()}</td>
+          <td>{this.renderNoteBox()}</td>
+          <td>{this.renderNoteBox()}</td>
+          <td>{this.renderNoteBox()}</td>
+          <td>{this.renderNoteBox()}</td>
+          <td>{this.renderNoteBox()}</td>
+          <td>{this.renderNoteBox()}</td>
+          </div>
+        </tr>
+      </div>
+    );
+  }
+}
+
+class Grid extends React.Component {
+
+
+  render() {
+    return (
+      <div className="Grid">
+        <table>
+          <GridRow />
+          <GridRow />
+          <GridRow />
+          <GridRow />
+          <GridRow />
+          <GridRow />
+          <GridRow />
+          <GridRow />
+        </table>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <Grid />,
+  document.getElementById('test')
+);
