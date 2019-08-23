e53606c70e25a510a516a09bd2de565070234414
thealienthing <benjamin.stoneking@gmail.com>
2019-08-22 21:21:59 -0600

    Added very important newline Co-authored-by: Rick <rick.johnson06@gmail.com>

--- src/Button.js
+++ src/Button.js
@@ -30,6 +30,7 @@ var seq = new Tone.Sequence(function(time, index){
 //subdivisions are given as subarrays
 }, [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]).start(0);
 
+
 class Button extends Component {
   render() {
     return(
