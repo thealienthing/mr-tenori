5cc850d57999dd4f3c3a1bd6bc7a8565bac6f016
thealienthing <benjamin.stoneking@gmail.com>
2019-08-29 22:30:55 -0600

    It works! The grid changes values for the tracks. Polyphonic synth reads in the selected values and plays back in a perfect loop. It only took an entire to write a stupid ass algorithm that built the time line. Added pingpong delay for flavor. Also added major pentatonic scale.
    Co-authored-by: Rick Johnson <rick.johnson06@gmail.com>
    Co-authored-by: Ben Stoneking <benjamin.stoneking@gmail.com>

--- src/Tenori.js
+++ src/Tenori.js
@@ -4,7 +4,8 @@ import Tone from 'tone';
 
 class SynthTrack {
   constructor(args) {
-    this.synth = new Tone.Synth().toMaster()
+    var pingPong = new Tone.PingPongDelay("4n", 0.2).toMaster();
+    this.synth = new Tone.PolySynth(16, Tone.Synth).connect(pingPong);
 
     this.activeFrequencies = [];
     this.activeMidiNotes = [];
@@ -13,10 +14,10 @@ class SynthTrack {
       this.activeMidiNotes.push([]);
     }
 
-    this.midiScale = [];
-    for(let i = 69; i < 85; i++){
-      this.midiScale.push(i);
-    }
+    this.midiScale = [69, 71, 73, 76, 78, 81, 83, 85, 88, 90, 93, 95, 97, 100, 102, 104];
+    // for(let i = 69; i < 85; i++){
+    //   this.midiScale.push(i);
+    // }
 
     this.freqScale = this.midiScale.map(number => Tone.Midi(number).toFrequency());
 
@@ -36,6 +37,10 @@ class SynthTrack {
     //console.log(this.activeFrequencies);
   }
 
+  playTick(tick) {
+    this.synth.triggerAttackRelease(this.activeFrequencies[tick], "16n");
+  }
+
   playBoop() {
     this.synth.triggerAttackRelease('C4', '8n');
   }
@@ -46,6 +51,31 @@ class Tenori extends Component {
     super(props)
     this.useSynth = this.useSynth.bind(this);
     this.track = new SynthTrack();
+
+    this.setupTransport();
+  }
+
+  setupTransport() {
+    let numberOfBeats = 16;
+    for(let i = 0; i < numberOfBeats; i++) {
+      Tone.Transport.schedule((time) => { this.track.playTick(i); }, this.ticksToMeasures(i));
+    }
+
+    Tone.Transport.loop = true;
+    Tone.Transport.loopStart = this.ticksToMeasures(0);
+    Tone.Transport.loopEnd = this.ticksToMeasures(16);
+  }
+
+  kickItOff() {
+    Tone.Transport.stop();
+    Tone.Transport.start("+0.1", "0:0:0");
+  }
+
+  ticksToMeasures(ticks) {
+    let beats = Math.floor(ticks / 4) % 4;
+    let measures = Math.floor(ticks / 16);
+    let sixteenths = ticks % 4;
+    return measures + ":" + beats + ":" + sixteenths;
   }
 
   useSynth(){
@@ -60,6 +90,7 @@ class Tenori extends Component {
         <button onClick={() => this.track.addNote(6,0) }>Add</button>
         <button onClick={() => this.track.removeNote(6,0) }>Remove</button>
         <button onClick={() => console.log(this.track.activeFrequencies[6])}>Show Notes</button>
+        <button onClick={this.kickItOff}>GOGGOGOOGOOOGOGOOGOGOG</button>
         <Grid
           id='grid'
           numberOfBeats={16}
