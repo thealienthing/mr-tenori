486daae5c361c6e6d2880cfb124bc2c0f376379d
thealienthing <benjamin.stoneking@gmail.com>
2019-08-29 19:13:35 -0600

    Not using Button.js. Was a nice way to test using tone.js but we've outgrown it. Time to move on to bigger and better things! Also removed residual calls to RawStrength.js (RIP).
    Co-authored-by: Rick Johnson <rick.johnson06@gmail.com>
    Co-authored-by: Ben Stoneking <benjamin.stoneking@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Button from './Button';
-import RawStrength from './RawStrength.js';
+//import Button from './Button';
 import Grid from './Grid.js';
 
 function AppContainer(props) {
 	return (
 		<React.Fragment>
-			<Button />
-			<RawStrength />
 			<Grid id='grid' numberOfBeats={16} numberOfNotes={16} />
 		</React.Fragment>
 	);
